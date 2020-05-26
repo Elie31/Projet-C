5736274f3f46beb17fa0240b93a9b59b9d10f073
Elie31 <elie31620@hotmail.fr>
2020-05-26 09:35:41 +0200

    décoder IP et fonction convertir marche pas

--- fonction.c
+++ fonction.c
@@ -5,96 +5,141 @@
 
 void formatAdresse(int a []);
 void conversion(int a[], int binaire[]);
-void decoderIp(int b[]);
+void decoderIp(int a[],int b[]);
 
 
 void main() {
-int a[4];
-int b[31];
-formatAdresse(a);
-conversion(a,b);
+	int a[4];
+	int b[31];
+	formatAdresse(a);
+	conversion(a,b);
+	decoderIp(a,b);
 }
+
 void formatAdresse(int a[]){
-unsigned ip1, ip2, ip3, ip4, ip5;
+	unsigned ip1, ip2, ip3, ip4, ip5;
 
-int erreur = 1;
-int erreur2 = 1;
+	int erreur = 1;
+	int erreur2 = 1;
 
 
-printf("entrer votre adresse ip : \n");
-while (erreur==1){
-if( scanf("%u.%u.%u.%u/%u" , &ip1 , &ip2, &ip3, &ip4, &ip5) != 5 ||
-    ip1>255 || ip2>255 || ip3>255 || ip4>255 || ip5>32)
-	printf("Saisie erronée, écriver sous la forme : 255.255.255.255/32\n");
-else
-	erreur=0;
+	printf("entrer votre adresse ip : \n");
+	while (erreur==1){
+		if( scanf("%u.%u.%u.%u/%u" , &ip1 , &ip2, &ip3, &ip4, &ip5) != 5 ||
+	    ip1>255 || ip2>255 || ip3>255 || ip4>255 || ip5>32){
+			printf("Saisie erronée, écriver sous la forme : 255.255.255.255/32\n");
+		}else{
+			erreur=0;
+		}
+	}
+
+
+	//printf("entrer votre masque réseau : (0 si il n'y a pas de masque) \n");
+	//while (erreur2==1){
+	//if( scanf("/%u" , &ip5) != 1 ||
+	//    ip5>32 )
+	//        printf("Saisie erronée, écriver sous la forme : 255.255.255.255\n");
+	//else
+	//        erreur2=0;
+	//}
+	//while (erreur2==1){
+	//if ( scanf("/%u",&ip5) != 1 || ip5>32)
+	//	printf("Saisie erronée, écriver sous la forme : '/24','/19'...\n");
+	//else
+	//	erreur2=0;
+	//}
+	a[0] = ip1;
+	a[1] = ip2;
+	a[2] = ip3;
+	a[3] = ip4;
+	a[4] = ip5;
+	printf("votre adresse ip est : %d.%d.%d.%d/%d\n",a[0],a[1],a[2],a[3],a[4]);
 }
-//printf("entrer votre masque réseau : (0 si il n'y a pas de masque) \n");
-//while (erreur2==1){
-//if( scanf("/%u" , &ip5) != 1 ||
-//    ip5>32 )
-//        printf("Saisie erronée, écriver sous la forme : 255.255.255.255\n");
-//else
-//        erreur2=0;
-//}
-//while (erreur2==1){
-//if ( scanf("/%u",&ip5) != 1 || ip5>32)
-//	printf("Saisie erronée, écriver sous la forme : '/24','/19'...\n");
-//else
-//	erreur2=0;
-//}
-a[0] = ip1;
-a[1] = ip2;
-a[2] = ip3;
-a[3] = ip4;
-a[4] = ip5;
-printf("votre adresse ip est : %d.%d.%d.%d/%d\n",a[0],a[1],a[2],a[3],a[4]);
-}
+
 
 void conversion(int a[], int binaire[]){
 
-int j=0;
+	int j=0;
 
-for(int i=0; i<4; i++)
-{
-	
-	while(a[i]!=0)
+	for(int i=0; i<4; i++)
 	{
-		binaire[j]=a[i]%2;
-		a[i]=a[i]/2;
-		j++;
+		
+		while(a[i]!=0){
+			binaire[j]=a[i]%2;
+			a[i]=a[i]/2;
+			j++;
+		}
+		if (j<8){
+			while (j<8){
+				binaire[j]=0;
+				j++;
+			}
+		}else{
+			if (j<16 & j>8){
+		     	   	while (j<16){
+					binaire[j]=0;
+					j++;
+				}
+			}else{
+				if (j<24 & j>16){
+		        		while (j<24){
+						binaire[j]=0;
+						j++;
+					}
+				}else{
+					if (j<32 & j>24){
+		        			while (j<32){
+							binaire[j]=0;
+							j++;
+						}
+							    
+					}
+				}
+			}
+		}
 	}
-	if (j<8)
-		while (j<8){
-		binaire[j]=0;
-		j++;}
-	else
-		if (j<16 & j>8)
-                	while (j<16){
-                	binaire[j]=0;
-                	j++;}
-		else
-			if (j<24 & j>16)
-                		while (j<24){
-                		binaire[j]=0;
-                		j++;}
-			else
-				if (j<32 & j>24)
-                			while (j<32){
-                			binaire[j]=0;
-                			j++;}
-}
 
-for(int k=0; k<32; k++){
-printf("%d,", binaire[k]);
-}
-
-void decoderIp(int b[]){
-
-
+	for(int k=0; k<32; k++){
+		printf("%d,", binaire[k]);
+	}
+		
 }
 
 
+void decoderIp(int a[],int b[]){
+	for(int i=0; i<3; i++){
+		if(b[0]==0){
+			printf("votre adresse ip est une classe A");
+			break;
+		}else{
+			if(b[0]==1 && b[1]==0){
+				printf("votre adresse ip est une classe B");
+				break;
+			}else{
+				if(b[0]==1 && b[1]==1 && b[2]==0){
+					printf("votre adresse ip est une classe C");
+					break;
+				}else{
+					if(b[0]==1 && b[1]==1 && b[2]==1 && b[3]==0){
+						printf("votre adresse ip est une classe D");
+						break;
+					}else{
+						if(b[0]==1 && b[1]==1 && b[2]==1 && b[3]==1){
+							printf("votre adresse ip est une classe E");
+							break;
+						}
+					}	
+				}				
+			}
+		}
+		
 
+	}
+		
 
 }
+
+
+
+
+
